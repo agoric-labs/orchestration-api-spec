15119290a5c722c6ff7949d3fa351eec0faa0780
Turadg Aleahmad <turadg@agoric.com>
2024-04-08 06:41:18 -0700

    chore(types): fix utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,20 @@
 import '@agoric/zoe/exported.js';
+import type { ChainAddress } from './orchestration';
 
-/** @type {import('./index').OrcUtils} */
+// TODO figure out ERTP requirements
+type Brand = unknown;
+type Amount<T extends 'nat' = 'nat'> = { brand: Brand; value: bigint };
+
+type AfterAction = { destChain: string; destAddress: ChainAddress };
+type SwapExact = { amountIn: Amount; amountOut: Amount };
+type SwapMaxSlippage = { amountIn: Amount; brandOut: Brand; slippage: number };
+export type OsmosisSwapArgs = (SwapExact | SwapMaxSlippage) &
+  (AfterAction | Record<string, never>);
+
 export const orcUtils = {
   /**
+   * unwinds denom with PFM, if necessary
+   *
    * @param {Omit<import('./index').TransferMsg, 'memo'>} _args
    * @returns {import('./index').TransferMsg}
    */
@@ -11,6 +23,7 @@ export const orcUtils = {
     return {};
   },
   /**
+   * SwapExact or SwapMaxSlippage, with optional AfterAction
    * @param {import('./index').OsmosisSwapArgs} _args
    * @returns {import('./index').TransferMsg}
    */
